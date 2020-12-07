bd741622cd659e98180d057d2daee0710aab9201
SaraZOUIOULA <s.zouioula@it-students.fr>
2020-12-07 17:59:06 +0100

    first commit

---
+++ js/contact.js
@@ -0,0 +1,8 @@
+
+document.getElementById("flex").addEventListener("submit",()=>{
+    window.alert("Votre formulaire a bien été envoyé");
+    document.getElementById("flex").style.display = 'none';
+    document.getElementById("message").innerHTML=("Votre formulaire a bien été envoyé");
+});
+
+   
